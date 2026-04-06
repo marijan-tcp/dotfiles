f4a86ea96dc5f20817dd2c12559dd817c55fe1e5
Matija Marijan <MMarijan@TCPSoftware.com>
2026-04-06 15:42:07 +0200

    Update pyright config, disable inline warnings

--- .config/nvim/lua/plugins/pyright.lua
+++ .config/nvim/lua/plugins/pyright.lua
@@ -3,15 +3,11 @@ return {
   opts = {
     servers = {
       pyright = {
-        handlers = {
-          ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
-            -- Discard all diagnostics from pyright
-          end,
-        },
         settings = {
           python = {
             analysis = {
               typeCheckingMode = "off",
+              diagnosticMode = "none",  -- or "OpenFilesOnly"
             },
           },
         },
